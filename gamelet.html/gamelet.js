619632480cfc4d2361585a0aa9567f29316f1f90
Zlayxa <john.c.wharton2@btinternet.com>
2025-05-12 16:26:27 +0100

    Just radnomly updated and deleted crap.

--- gamelet.html/gamelet.js
+++ gamelet.html/gamelet.js
@@ -29,3 +29,30 @@ function handleKeyPress(e) {
 function refresh() {
   ball.style.left = position + "px";
 }
+
+let phoneNumber;
+phoneNumber = "07000-000000";
+/* 'let' is the function, 'phoneNumber' is the variable.
+This gives 'phoneNumber' a value as a variable instead of being just an 'undefined' variable.
+Giving a value to a variable for the first time is callled initialising a variable. */
+
+let favouriteColour = "red";
+// You can also declare and initialise a variable in the same line, like above, instead of doing it in two lines like with 'phoneNumber'.
+
+const myArray = new Array[
+  ("January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December")
+]();
+
+let customerData = "Barb Seibert, 3739 Sheila Lane, Goldfield, NV, 89013";
+const customerDataArray = customerData.split(",");
